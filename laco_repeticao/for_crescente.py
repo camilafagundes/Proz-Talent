81b4e684036247b38ec94f9f1ada9a1c8c87c2fa
camilafagundes <fagundescamilaferraz@gmail.com>
2025-01-06 10:16:58 -0300

    teste

---
+++ laco_repeticao/for_crescente.py
@@ -0,0 +1,4 @@
+for andar in range (0,21): #O laço para exatamente no número solicitado,sendo assim, o mesmo não aparece na tela
+    if andar ==13:
+        continue
+    print(andar)
---
+++ laco_repeticao/while_crescente.py
@@ -0,0 +1,6 @@
+andar = 1
+while andar <= 20:
+    if andar != 13:  # Imprima apenas se não for 13
+        print(andar)
+    andar +=1
+    
