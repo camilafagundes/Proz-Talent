632462b8a92d3fa187d6f3bda99cbd9232bb39f1
camilafagundes <fagundescamilaferraz@gmail.com>
2025-04-18 13:23:09 -0300

    exercício 2

---
+++ js1-modulo5/OneCompiler.js
@@ -0,0 +1,9 @@
+function operacoesBasicas(a, b) {
+    console.log(`${a} + ${b} = ${a + b}`);
+    console.log(`${a} - ${b} = ${a - b}`);
+    console.log(`${a} x ${b} = ${a * b}`);
+    console.log(`${a} / ${b} = ${a / b}`);
+  }
+  
+  operacoesBasicas(4, 5);
+  
